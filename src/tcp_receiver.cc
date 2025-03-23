1f6b254641b81ebcfc80749b5357676fa5c5dbca
deer <1033059910@qq.com>
2025-03-23 16:42:28 +0800

    warp对应包装，比较简单，unwrap对应解包装，根据指导书，最接近checkpoint的那个点就是对应的绝对序列号。要注意绝对序列号与字节流索引之间转换。在send函数中要注意FIN也要占据一个序列号，FIN报文是否被完全处理，取决于写流是否关闭，因此可以在最后＋isclosed()。rst信号根据函数has_error发送。这里还经常会用到数据类型超过位宽自动取模

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,18 +1,32 @@
 #include "tcp_receiver.hh"
+
 #include "debug.hh"
 
 using namespace std;
 
-void TCPReceiver::receive( TCPSenderMessage message )
-{
-  // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+void TCPReceiver::receive(TCPSenderMessage message) {
+    if (message.RST) {
+        reassembler_.reader().set_error();
+    }
+    if (message.SYN) {
+        zero_point.emplace(message.seqno);
+        reassembler_.insert(0, message.payload, message.FIN);
+    } else if (zero_point != std::nullopt) {
+        uint64_t first_index =
+            message.seqno.unwrap(zero_point.value(), reassembler_.writer().bytes_pushed()) - 1;
+        reassembler_.insert(first_index, message.payload, message.FIN);
+    }
 }
 
-TCPReceiverMessage TCPReceiver::send() const
-{
-  // Your code here.
-  debug( "unimplemented send() called" );
-  return {};
+TCPReceiverMessage TCPReceiver::send() const {
+    // ACK号以及滑动窗口大小
+    uint16_t window_size = reassembler_.writer().available_capacity() > (uint64_t)65535
+                               ? 65535
+                               : reassembler_.writer().available_capacity();
+    if (zero_point == std::nullopt) {
+        return {std::nullopt, window_size, reassembler_.reader().has_error()};
+    }
+    return {std::optional<Wrap32>(Wrap32::wrap(reassembler_.writer().bytes_pushed() + 1, zero_point.value()) +
+                                  reassembler_.writer().is_closed()),
+            window_size, reassembler_.reader().has_error()};
 }
--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -1,30 +1,32 @@
 #pragma once
 
+#include <optional>
+
 #include "reassembler.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+class TCPReceiver {
+   public:
+    // Construct with given Reassembler
+    explicit TCPReceiver(Reassembler&& reassembler)
+        : reassembler_(std::move(reassembler)), zero_point{std::nullopt} {}
 
-class TCPReceiver
-{
-public:
-  // Construct with given Reassembler
-  explicit TCPReceiver( Reassembler&& reassembler ) : reassembler_( std::move( reassembler ) ) {}
+    /*
+     * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
+     * at the correct stream index.
+     */
+    void receive(TCPSenderMessage message);
 
-  /*
-   * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
-   * at the correct stream index.
-   */
-  void receive( TCPSenderMessage message );
+    // The TCPReceiver sends TCPReceiverMessages to the peer's TCPSender.
+    TCPReceiverMessage send() const;
 
-  // The TCPReceiver sends TCPReceiverMessages to the peer's TCPSender.
-  TCPReceiverMessage send() const;
+    // Access the output
+    const Reassembler& reassembler() const { return reassembler_; }
+    Reader& reader() { return reassembler_.reader(); }
+    const Reader& reader() const { return reassembler_.reader(); }
+    const Writer& writer() const { return reassembler_.writer(); }
 
-  // Access the output
-  const Reassembler& reassembler() const { return reassembler_; }
-  Reader& reader() { return reassembler_.reader(); }
-  const Reader& reader() const { return reassembler_.reader(); }
-  const Writer& writer() const { return reassembler_.writer(); }
-
-private:
-  Reassembler reassembler_;
+   private:
+    Reassembler reassembler_;
+    std::optional<Wrap32> zero_point;
 };
