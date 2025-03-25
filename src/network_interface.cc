62a0bc8fcef96e025ffd71c1a8dcfa1f0308eb54
deer <1033059910@qq.com>
2025-03-25 22:36:36 +0800

    完成了实验5的一部分，这块要注意使用不同数据结构，删除元素以后迭代器是否会失效

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -1,3 +1,5 @@
+#include "network_interface.hh"
+
 #include <iostream>
 
 #include "arp_message.hh"
@@ -5,45 +7,163 @@
 #include "ethernet_frame.hh"
 #include "exception.hh"
 #include "helpers.hh"
-#include "network_interface.hh"
 
 using namespace std;
 
 //! \param[in] ethernet_address Ethernet (what ARP calls "hardware") address of the interface
 //! \param[in] ip_address IP (what ARP calls "protocol") address of the interface
-NetworkInterface::NetworkInterface( string_view name,
-                                    shared_ptr<OutputPort> port,
-                                    const EthernetAddress& ethernet_address,
-                                    const Address& ip_address )
-  : name_( name )
-  , port_( notnull( "OutputPort", move( port ) ) )
-  , ethernet_address_( ethernet_address )
-  , ip_address_( ip_address )
-{
-  cerr << "DEBUG: Network interface has Ethernet address " << to_string( ethernet_address_ ) << " and IP address "
-       << ip_address.ip() << "\n";
+
+bool NetworkInterface::equal(const EthernetAddress& d1, const EthernetAddress& d2) {
+    for (int i = 0; i < 6; i++) {
+        if (d1[i] != d2[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
+
+NetworkInterface::NetworkInterface(string_view name, shared_ptr<OutputPort> port,
+                                   const EthernetAddress& ethernet_address, const Address& ip_address)
+    : name_(name),
+      port_(notnull("OutputPort", move(port))),
+      ethernet_address_(ethernet_address),
+      ip_address_(ip_address),
+      arp_map_(),
+      wait_arp_msgs(),
+      cache_queue_(),
+      time_(0) {}
 
 //! \param[in] dgram the IPv4 datagram to be sent
-//! \param[in] next_hop the IP address of the interface to send it to (typically a router or default gateway, but
-//! may also be another host if directly connected to the same network as the destination) Note: the Address type
-//! can be converted to a uint32_t (raw 32-bit IP address) by using the Address::ipv4_numeric() method.
-void NetworkInterface::send_datagram( const InternetDatagram& dgram, const Address& next_hop )
-{
-  debug( "unimplemented send_datagram called" );
-  (void)dgram;
-  (void)next_hop;
+//! \param[in] next_hop the IP address of the interface to send it to (typically a router or default gateway,
+//! but may also be another host if directly connected to the same network as the destination) Note: the
+//! Address type can be converted to a uint32_t (raw 32-bit IP address) by using the Address::ipv4_numeric()
+//! method.
+void NetworkInterface::send_datagram(const InternetDatagram& dgram, const Address& next_hop) {
+    uint32_t next_hop_ipv4Addr = next_hop.ipv4_numeric();
+    auto it = arp_map_.find(next_hop_ipv4Addr);
+    if (it != arp_map_.end() && time_ - it->second.second < 30 * 1000) {
+        // 存在映射，直接构造帧发送
+        EthernetHeader frame_head = {it->second.first, ethernet_address_, EthernetHeader::TYPE_IPv4};
+        EthernetFrame frame = {frame_head, serialize(dgram)};
+        transmit(frame);
+    } else {
+        // 缓存发送数据)
+        if (wait_arp_msgs.find(next_hop_ipv4Addr) == wait_arp_msgs.end()) {
+            // 不存在映射，发送ARP请求
+            EthernetHeader frame_head = {ETHERNET_BROADCAST, ethernet_address_, EthernetHeader::TYPE_ARP};
+            // 创建arp消息
+            ARPMessage arp_msg;
+            arp_msg.opcode = ARPMessage::OPCODE_REQUEST;
+            arp_msg.sender_ethernet_address = ethernet_address_;
+            arp_msg.sender_ip_address = ip_address_.ipv4_numeric();
+            arp_msg.target_ethernet_address = {0};
+            arp_msg.target_ip_address = next_hop_ipv4Addr;
+            EthernetFrame frame = {frame_head, serialize(arp_msg)};
+            transmit(frame);
+            wait_arp_msgs[next_hop_ipv4Addr] = time_;
+            cache_queue_.push_back(std::make_pair(next_hop, dgram));
+        } else {
+            for (auto it2 = cache_queue_.begin(); it2 != cache_queue_.end(); it2++) {
+                if (it2->first.ipv4_numeric() == next_hop_ipv4Addr &&
+                    it2->second.header.src == dgram.header.src &&
+                    it2->second.header.dst == dgram.header.dst) {
+                    return;
+                }
+            }
+            cache_queue_.push_back(std::make_pair(next_hop, dgram));
+        }
+    }
 }
-
 //! \param[in] frame the incoming Ethernet frame
-void NetworkInterface::recv_frame( EthernetFrame frame )
-{
-  debug( "unimplemented recv_frame called" );
-  (void)frame;
+void NetworkInterface::recv_frame(EthernetFrame frame) {
+    EthernetHeader header = frame.header;
+    // 只接受以太网目的地是广播地址或存储在以太网地址成员变量`_ethernet_address`中的以太网地址
+    if (!equal(header.dst, ETHERNET_BROADCAST) && !equal(header.dst, ethernet_address_)) {
+        return;
+    }
+    if (frame.header.type == EthernetHeader::TYPE_IPv4) {
+        InternetDatagram ip_datagram;
+        auto res = parse(ip_datagram, move(frame.payload));
+        if (res == true) {
+            datagrams_received_.push(ip_datagram);
+        } else {
+            return;
+        }
+    } else {
+        ARPMessage arp_msg;
+        auto res = parse(arp_msg, move(frame.payload));
+        if (res == true) {
+            // 发送方以太网地址和ip地址
+            uint32_t ip_addr = arp_msg.sender_ip_address;
+            // 此外，如果是ARP请求请求我们的IP地址，请发送适当的ARP回复。
+            if ((arp_msg.opcode == ARPMessage::OPCODE_REQUEST) &&
+                (arp_msg.target_ip_address == ip_address_.ipv4_numeric())) {
+                EthernetHeader frame_head = {arp_msg.sender_ethernet_address, ethernet_address_,
+                                             EthernetHeader::TYPE_ARP};
+                ARPMessage arp_msg_send;
+                arp_msg_send.opcode = arp_msg_send.OPCODE_REPLY;
+                arp_msg_send.sender_ethernet_address = ethernet_address_;
+                arp_msg_send.sender_ip_address = ip_address_.ipv4_numeric();
+                arp_msg_send.target_ethernet_address = arp_msg.sender_ethernet_address;
+                arp_msg_send.target_ip_address = arp_msg.sender_ip_address;
+
+                // 发送
+                EthernetFrame sendframe = {frame_head, serialize(arp_msg_send)};
+                transmit(sendframe);
+            }
+            // 更新映射
+            arp_map_[ip_addr] = {arp_msg.sender_ethernet_address, time_};
+            // 发送
+            for (auto it = cache_queue_.begin(); it != cache_queue_.end();) {
+                Address address_cache = it->first;
+                InternetDatagram dgram_cache = it->second;
+                // 如果ip是更新的ip, 则发送
+                if (address_cache.ipv4_numeric() == ip_addr) {
+                    send_datagram(dgram_cache, address_cache);
+                    // 删除
+                    cache_queue_.erase(it);
+                } else {
+                    it++;
+                }
+            }
+            // 删除已发送未回应的部分
+            wait_arp_msgs.erase(ip_addr);
+        }
+        return;
+    }
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void NetworkInterface::tick( const size_t ms_since_last_tick )
-{
-  debug( "unimplemented tick({}) called", ms_since_last_tick );
+void NetworkInterface::tick(const size_t ms_since_last_tick) {
+    time_ += ms_since_last_tick;
+    // 更新映射表
+    for (auto it = arp_map_.begin(); it != arp_map_.end();) {
+        // 超过30秒则删除
+        if (time_ - (it->second).second >= 30 * 1000) {
+            arp_map_.erase(it++);
+        } else {
+            it++;
+        }
+    }
+    // 更新已发送, 未回应的部分
+    for (auto it = wait_arp_msgs.begin(); it != wait_arp_msgs.end(); it++) {
+        // 超过5秒则重发
+        if (time_ - it->second >= 5 * 1000) {
+            // 不在arp表中, 广播
+            EthernetHeader frame_head = {ETHERNET_BROADCAST, ethernet_address_, EthernetHeader::TYPE_ARP};
+            // 创建arp消息
+            ARPMessage arp_msg;
+            arp_msg.opcode = ARPMessage::OPCODE_REQUEST;
+            arp_msg.sender_ethernet_address = ethernet_address_;
+            arp_msg.sender_ip_address = ip_address_.ipv4_numeric();
+            arp_msg.target_ethernet_address = {0};
+            arp_msg.target_ip_address = it->first;
+            EthernetFrame frame = {frame_head, serialize(arp_msg)};
+            // 发送
+            transmit(frame);
+            // 更新发送时间
+            it->second = time_;
+        }
+    }
 }
--- src/network_interface.hh
+++ src/network_interface.hh
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <deque>
+#include <list>
+#include <memory>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
 #include "address.hh"
 #include "ethernet_frame.hh"
 #include "ipv4_datagram.hh"
-
-#include <memory>
-#include <queue>
 
 // A "network interface" that connects IP (the internet layer, or network layer)
 // with Ethernet (the network access layer, or link layer).
@@ -28,58 +32,65 @@
 // the network interface passes it up the stack. If it's an ARP
 // request or reply, the network interface processes the frame
 // and learns or replies as necessary.
-class NetworkInterface
-{
-public:
-  // An abstraction for the physical output port where the NetworkInterface sends Ethernet frames
-  class OutputPort
-  {
-  public:
-    virtual void transmit( const NetworkInterface& sender, const EthernetFrame& frame ) = 0;
-    virtual ~OutputPort() = default;
-  };
+class NetworkInterface {
+   public:
+    // An abstraction for the physical output port where the NetworkInterface sends Ethernet frames
+    class OutputPort {
+       public:
+        virtual void transmit(const NetworkInterface& sender, const EthernetFrame& frame) = 0;
+        virtual ~OutputPort() = default;
+    };
 
-  // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
-  // addresses
-  NetworkInterface( std::string_view name,
-                    std::shared_ptr<OutputPort> port,
-                    const EthernetAddress& ethernet_address,
-                    const Address& ip_address );
+    // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
+    // addresses
+    NetworkInterface(std::string_view name, std::shared_ptr<OutputPort> port,
+                     const EthernetAddress& ethernet_address, const Address& ip_address);
 
-  // Sends an Internet datagram, encapsulated in an Ethernet frame (if it knows the Ethernet destination
-  // address). Will need to use [ARP](\ref rfc::rfc826) to look up the Ethernet destination address for the next
-  // hop. Sending is accomplished by calling `transmit()` (a member variable) on the frame.
-  void send_datagram( const InternetDatagram& dgram, const Address& next_hop );
+    // Sends an Internet datagram, encapsulated in an Ethernet frame (if it knows the Ethernet destination
+    // address). Will need to use [ARP](\ref rfc::rfc826) to look up the Ethernet destination address for the
+    // next hop. Sending is accomplished by calling `transmit()` (a member variable) on the frame.
+    void send_datagram(const InternetDatagram& dgram, const Address& next_hop);
 
-  // Receives an Ethernet frame and responds appropriately.
-  // If type is IPv4, pushes the datagram to the datagrams_in queue.
-  // If type is ARP request, learn a mapping from the "sender" fields, and send an ARP reply.
-  // If type is ARP reply, learn a mapping from the "sender" fields.
-  void recv_frame( EthernetFrame frame );
+    // Receives an Ethernet frame and responds appropriately.
+    // If type is IPv4, pushes the datagram to the datagrams_in queue.
+    // If type is ARP request, learn a mapping from the "sender" fields, and send an ARP reply.
+    // If type is ARP reply, learn a mapping from the "sender" fields.
+    void recv_frame(EthernetFrame frame);
 
-  // Called periodically when time elapses
-  void tick( size_t ms_since_last_tick );
+    // Called periodically when time elapses
+    void tick(size_t ms_since_last_tick);
 
-  // Accessors
-  const std::string& name() const { return name_; }
-  const OutputPort& output() const { return *port_; }
-  OutputPort& output() { return *port_; }
-  std::queue<InternetDatagram>& datagrams_received() { return datagrams_received_; }
+    // Accessors
+    const std::string& name() const { return name_; }
+    const OutputPort& output() const { return *port_; }
+    OutputPort& output() { return *port_; }
+    std::queue<InternetDatagram>& datagrams_received() { return datagrams_received_; }
 
-private:
-  // Human-readable name of the interface
-  std::string name_;
+   private:
+    // Human-readable name of the interface
+    std::string name_;
 
-  // The physical output port (+ a helper function `transmit` that uses it to send an Ethernet frame)
-  std::shared_ptr<OutputPort> port_;
-  void transmit( const EthernetFrame& frame ) const { port_->transmit( *this, frame ); }
+    // The physical output port (+ a helper function `transmit` that uses it to send an Ethernet frame)
+    std::shared_ptr<OutputPort> port_;
+    void transmit(const EthernetFrame& frame) const { port_->transmit(*this, frame); }
 
-  // Ethernet (known as hardware, network-access-layer, or link-layer) address of the interface
-  EthernetAddress ethernet_address_;
+    // Ethernet (known as hardware, network-access-layer, or link-layer) address of the interface
+    EthernetAddress ethernet_address_;
 
-  // IP (known as internet-layer or network-layer) address of the interface
-  Address ip_address_;
+    // IP (known as internet-layer or network-layer) address of the interface
+    Address ip_address_;
 
-  // Datagrams that have been received
-  std::queue<InternetDatagram> datagrams_received_ {};
+    // Datagrams that have been received
+    std::queue<InternetDatagram> datagrams_received_{};
+
+   private:
+    // arp映射表
+    std::unordered_map<uint32_t, std::pair<EthernetAddress, uint64_t>> arp_map_;
+    // 发出去但是未得到响应的arp消息（只需要记录目标ip地址即可）,同时记录时间，超时后重新发送
+    std::unordered_map<uint32_t, uint64_t> wait_arp_msgs;
+    // 缓存队列，缓存由于目标mac地址未知暂时没有发送的 ip数据报
+    std::list<std::pair<Address, IPv4Datagram>> cache_queue_;
+    // 时间变量
+    uint64_t time_;
+    bool equal(const EthernetAddress& d1, const EthernetAddress& d2);
 };
